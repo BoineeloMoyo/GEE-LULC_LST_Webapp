2c3007dfa3021cbd2bd72dbd46d3941c9c373279
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-02-27 19:07:01 +0100

    add description section

---
+++ gee_webapp.js
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * Model *
+ *
+ * A section to define information about the data being presented in your
+ * app.
+ *
+ * Guidelines: Use this section to import assets and define information that
+ * is used to parameterize data-dependant widgets and control style and
+ * behavior on UI interactions.
+ ******************************************************************************/
