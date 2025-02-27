f06e2b86051a2e9839bb9a1a488818ffbd2348d8
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-02-27 19:18:54 +0100

    define data model json

--- gee_webapp.js
+++ gee_webapp.js
@@ -19,3 +19,53 @@ var lst10: Image users/Boineelo_Moye/LST2010
 var lst15: Image users/Boineelo_Moye/LST2015
 var lst20: Image users/Boineelo_Moye/LST2020
  ******************************************************************************/
+ 
+// Define a JSON object for storing the data model.
+var m = {};
+
+
+m.datasets = {
+  'lulc': {
+    coll: ee.ImageCollection([
+      ee.Image('users/Boineelo_Moyo/classified2005').set('year', 2005), 
+      ee.Image('users/Boineelo_Moyo/classified2010').set('year', 2010), 
+      ee.Image('users/Boineelo_Moyo/classified2015').set('year', 2015).updateMask(ee.Image('users/Boineelo_Moyo/classified2015').neq(0)), 
+      ee.Image('users/Boineelo_Moyo/classified2020').set('year', 2020).updateMask(ee.Image('users/Boineelo_Moyo/classified2020').neq(0)), 
+    ]),
+    vis: {
+      min: 1,
+      max: 6,
+      palette: ["#42f132", "#81485c", "#117a17", "#54d4ff", "#f5deb7", "#cbe77e"]
+    },
+    classNames: ['Agric Lands','Built Up','Dense Vegetation',
+            'Water', 'Bare Soil', 'Mixed Grassland']    
+  }, 
+  'lst': {
+    coll: ee.ImageCollection([
+      ee.Image('users/Boineelo_Moyo/LST2005').set('year', 2005), 
+      ee.Image('users/Boineelo_Moyo/LST2010').set('year', 2010).clip(studyarea), 
+      ee.Image('users/Boineelo_Moyo/LST2015').set('year', 2015), 
+      ee.Image('users/Boineelo_Moyo/LST2020').set('year', 2020), 
+    ]),
+    vis: {
+      min_constant: 25, max_constant: 43, 
+      min2005:25, max2005:44,
+      min2010:25, max2010:42,
+      min2015:29, max2015:42,
+      min2020:25, max2020:42,
+      palette: ['blue', 'cyan', 'green', 'yellow', 'red']
+    }
+  },
+  'lst_daily': {
+    coll: ee.ImageCollection("MODIS/006/MOD11A2"),
+    band: 'LST_Day_1km',
+    startDate: '2004-11-01',
+    endDate: '2020-11-01',
+  },
+  'aoi': {
+    coll: ee.FeatureCollection("users/Boineelo_Moyo/StudyArea"), lon: 26, lat: -24.6518, zoom: 12
+  }
+};
+
+m.years = ['2005', '2010', '2015', '2020'];
+
