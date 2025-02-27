9f5ee6e71a516a75856478b7c6b56566dcbc25ac
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-02-27 19:08:24 +0100

    refeine comment header

--- gee_webapp.js
+++ gee_webapp.js
@@ -1,7 +1,9 @@
 /*******************************************************************************
  * A section to define information about the data being presented in your app.
  *
- * Guidelines: Use this section to import assets and define information that
+ * Use this section to import assets and define information that
  * is used to parameterize data-dependant widgets and control style and
  * behavior on UI interactions.
+ * 
+ * 
  ******************************************************************************/
