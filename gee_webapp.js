52609b4a8daa31874ccb4779c67de40cf5322b58
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-02-27 19:12:05 +0100

    add variables

--- gee_webapp.js
+++ gee_webapp.js
@@ -4,6 +4,12 @@
  * Use this section to import assets and define information that
  * is used to parameterize data-dependant widgets and control style and
  * behavior on UI interactions.
- * 
- * 
+ *  
+
  ******************************************************************************/
+/* Define variables :- that is your classified images, study_area/aoi etc
+
+
+
+
+*/
