4e3b5c980958f3345e8d9b4ae7f6b88911cd309c
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-02-27 19:07:34 +0100

    update header

--- gee_webapp.js
+++ gee_webapp.js
@@ -1,8 +1,7 @@
 /*******************************************************************************
  * Model *
  *
- * A section to define information about the data being presented in your
- * app.
+ * A section to define information about the data being presented in your app.
  *
  * Guidelines: Use this section to import assets and define information that
  * is used to parameterize data-dependant widgets and control style and
